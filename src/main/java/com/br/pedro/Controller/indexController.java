659b6cb8b56e5a9fb869ac53e9ed1e10f82e9a9f
Pedro Luis <cl-pedro@hotmail.com>
2022-04-22 17:30:05 -0300

    ProvaSpring

---
+++ src/main/java/com/br/pedro/Controller/indexController.java
@@ -0,0 +1,13 @@
+package com.br.pedro.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+	@RequestMapping(value="/")
+	public String index() {
+		return "/Index";
+	}
+}
